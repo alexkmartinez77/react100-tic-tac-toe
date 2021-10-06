ba5aff31d7c82d6c9e33e9b98e307a3db885465e
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-06 13:58:46 -0700

    Corrects typo.

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ class Game extends React.Component {
   }
 
   handleClick(i){
-    const history = this.state.history.slice(0, this.state.stepNumber +1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length-1];
     const squares = current.squares.slice();
 
@@ -79,9 +79,9 @@ class Game extends React.Component {
     });    
   }
 
-  jumptTo(step) {
+  jumpTo(step) {
     this.setState({
-      stepnumber: step,
+      stepNumber: step,
       XIsNext: (step % 2) === 0,
           })
   }
